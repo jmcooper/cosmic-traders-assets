26dc040c7e6981784c4b84f5a573ddc74d4a3d21
jmcooper <jim@cooperplanet.com>
2021-02-08 17:06:16 -0700

    Update currency function to use GCR

--- src/utils/format-utils.js
+++ src/utils/format-utils.js
@@ -1,5 +1,5 @@
 const toCurrencyFormat = (value) => {
-  return value.toLocaleString('en-US', {style: 'currency', currency: 'USD'})
+  return value.toLocaleString('en-US', {minimumFractionDigits: 0, maximumFractionDigits: 0}) + ' GCR'
 }
 
 export { toCurrencyFormat }
